94749c7f0e2c0e562d8778bb04093c6d0fd5d0ae
John Mertic <jmertic@linuxfoundation.org>
2024-07-28 09:54:36 -0400

    Change to using a config file
    Signed-off-by: John Mertic <jmertic@linuxfoundation.org>

--- maintainers.mjs
+++ maintainers.mjs
@@ -2,101 +2,126 @@ import { Octokit } from "@octokit/core";
 import { paginateGraphQL } from "@octokit/plugin-paginate-graphql";
 import { Command } from 'commander';
 const program = new Command();
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const fs = require('fs');
+const yaml = require('js-yaml');
 
 program
   .name('GitHub Maintainers')
-  .description('CLI to get maintainers for a given GitHub org or repo')
+  .description('CLI to get maintainers given a defined config file')
   .version('0.0.1')
   .requiredOption('-g, --gh-api-key <value>', 'GitHub API key')
-  .requiredOption('-o, --org <value>', 'GitHub Organization')
-  .option('-r, --repo <value>', 'Github Repository')
+  .requiredOption('-c, --config <value>', 'Configuration file')
   .option('--output-csv <value>', 'Github Repository','output.csv');
 
 program.parse();
 const options = program.opts();
 const MyOctokit = Octokit.plugin(paginateGraphQL);
 const octokit = new MyOctokit({ auth: options.ghApiKey });
-
-var querystring = " org:".concat(options.org) 
-if ( options.repo ) {
-  querystring = " repo:".concat(options.org,'/',options.repo) 
+const checkdate = Intl.DateTimeFormat('en-CA').format(new Date().setFullYear(new Date().getFullYear() - 1))
+var data = new Map()
+var doc;
+try {
+  var doc = yaml.load(fs.readFileSync(options.config, 'utf8'));
+} catch (e) {
+  console.log(e);
 }
-var checkdate = Intl.DateTimeFormat('en-CA').format(new Date().setFullYear(new Date().getFullYear() - 1))
-const pageIterator = octokit.graphql.paginate.iterator(
-`query paginate($cursor: String){
-  search(query: "${querystring} is:pr merged:>${checkdate}", type: ISSUE, first:100, after: $cursor) {
-    edges {
-      node {
-        ... on PullRequest {
-          url 
-          mergedAt
-          mergedBy {
-            login
-            ... on User {
-              email
-              name
+
+for ( let x in doc ) {
+  console.log(`Loading projects for ${doc[x]['umbrella']}`)
+  var umbrella = new Map()
+  for ( let projectName in doc[x]['projects']) {
+    console.log(`Processing ${doc[x]['projects'][projectName]['project']}`)
+    var project = new Map()
+    for ( let repoURL in doc[x]['projects'][projectName]['repos'] ) {
+      console.log(`Getting maintainers for ${doc[x]['projects'][projectName]['repos'][repoURL]}`)
+      var uri = doc[x]['projects'][projectName]['repos'][repoURL].replace('https://github.com/','');
+      var [org, repo] = uri.split("/");
+      var querystring = " org:".concat(org) 
+      if ( repo ) {
+        querystring = " repo:".concat(org,'/',repo) 
+      }
+      const pageIterator = octokit.graphql.paginate.iterator(
+      `query paginate($cursor: String){
+        search(query: "${querystring} is:pr merged:>${checkdate}", type: ISSUE, first:100, after: $cursor) {
+          edges {
+            node {
+              ... on PullRequest {
+                url 
+                mergedAt
+                mergedBy {
+                  login
+                  ... on User {
+                    email
+                    name
+                  }
+                }
+                repository {
+                  name
+                  url
+                }
+              }
             }
           }
-          repository {name}
+          pageInfo {
+            hasNextPage
+            endCursor
+          }
+        }
+      }`,
+      );
+      var counter = 1
+      for await (const response of pageIterator) {
+        console.log(`Loading first ${100*counter++} records...`)
+
+        const prs = response.search.edges
+        for (let i = 0; i < prs.length; i++) {
+          var pr = null
+          if (project.has(prs[i].node.mergedBy.login)) {
+            pr = project.get(prs[i].node.mergedBy.login)
+            pr.set('count',pr.get('count')+1)
+            if (pr.get('firstMerged')>prs[i].node.mergedAt) {
+              pr.set('firstMerged',prs[i].node.mergedAt)
+            }
+            if (pr.get('lastMerged')<prs[i].node.mergedAt) {
+              pr.set('lastMerged',prs[i].node.mergedAt)
+            }
+            var repolist = pr.get('repos')
+            repolist.push(prs[i].node.repository.url.replace('https://github.com/',''))
+            pr.set('repos',repolist)
+          }
+          else {
+            pr = new Map()
+            
+            pr.set('lastMerged',prs[i].node.mergedAt)
+            pr.set('name',prs[i].node.mergedBy.name)
+            pr.set('email',prs[i].node.mergedBy.email)
+            pr.set('firstMerged',prs[i].node.mergedAt)
+            pr.set('count',1)
+            pr.set('repos', [prs[i].node.repository.url.replace('https://github.com/','')])
+          }
+          project.set(prs[i].node.mergedBy.login,pr)
         }
       }
     }
-    pageInfo {
-      hasNextPage
-      endCursor
-    }
+    umbrella.set(doc[x]['projects'][projectName]['project'],project)  
   }
-}`,
-);
-var data = new Map()
-
-if ( options.repo ) {
-  console.log("Maintainers for ".concat(options.org,'/',options.repo))
+  data.set(doc[x]['umbrella'],umbrella)
 }
-else {
-  console.log("Maintainers for ".concat(options.org))
-}
-console.log("Starting to load records")
-var counter = 1
-for await (const response of pageIterator) {
-  console.log(`Loading first ${100*counter++} records...`)
 
-  const prs = response.search.edges
-  for (let i = 0; i < prs.length; i++) {
-    var pr = null
-    if (data.has(prs[i].node.mergedBy.login)) {
-      pr = data.get(prs[i].node.mergedBy.login)
-      pr.set('count',pr.get('count')+1)
-      if (pr.get('firstMerged')>prs[i].node.mergedAt) {
-        pr.set('firstMerged',prs[i].node.mergedAt)
-      }
-      if (pr.get('lastMerged')<prs[i].node.mergedAt) {
-        pr.set('lastMerged',prs[i].node.mergedAt)
-      }
-        var repolist = pr.get('repos')
-        repolist.push(prs[i].node.repository.name)
-        pr.set('repos',repolist)
+console.log("Converting to CSV file")
+
+var outputcsv = 'Umbrella,Project,GitHubID,Name,Email,FirstMerge,LastMerge,Count,Repos\n'
+for (let [umbrella, projects] of data) {
+  for (let [projectName, project] of projects) {
+    for (let [maintainerName,maintainer] of project) {
+      outputcsv = outputcsv.concat(`"${umbrella}","${projectName}","${maintainerName}","${maintainer.get("name")}","${maintainer.get('email')}","${maintainer.get('firstMerged')}","${maintainer.get('lastMerged')}","${maintainer.get('count')}","${[...new Set(maintainer.get('repos'))].join()}"\n`)
+
     }
-    else {
-      pr = new Map()
-      pr.set('lastMerged',prs[i].node.mergedAt)
-      pr.set('name',prs[i].node.mergedBy.name)
-      pr.set('email',prs[i].node.mergedBy.email)
-      pr.set('firstMerged',prs[i].node.mergedAt)
-      pr.set('count',1)
-      pr.set('repos', [prs[i].node.repository.name])
-    }
-    data.set(prs[i].node.mergedBy.login,pr)
   }
 }
 
-var outputcsv = 'GitHubID,Name,EmailFirstMerge,LastMerge,Count,Repos\n'
-for (let [key, value] of data) {
-  outputcsv = outputcsv.concat(`"${key}","${value.get('name')}","${value.get('email')}","${value.get('firstMerged')}","${value.get('lastMerged')}","${value.get('count')}","${[...new Set(value.get('repos'))].join()}"\n`)
-}
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-var fs = require('fs');
 fs.writeFile(options.outputCsv, outputcsv, 'utf8', function (err) {
   if (err) {
     console.log('Some error occured - file either not saved or corrupted file saved.');
